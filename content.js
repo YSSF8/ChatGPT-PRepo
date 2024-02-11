8228018fc2ab7b6725cccf9edb9ef9c422b020a1
YSSF8 <76586183+YSSF8@users.noreply.github.com>
2024-02-11 13:46:02 +0100

    Add files via upload

--- content.js
+++ content.js
@@ -1,9 +1,19 @@
-chrome.runtime.onMessage.addListener(message => {
-    if (message.action == 'overrideElement') {
-        const input = document.getElementById('prompt-textarea');
-        input.focus();
-        input.value = message.prompt.replace(/^[ \t]+/gm, '').trim();
-        document.execCommand('selectAll', false, null);
-        document.execCommand('insertText', false, input.value);
-    }
-});
+if (!document.body.classList.contains('script-injected')) {
+    document.body.classList.add('script-injected');
+
+    chrome.runtime.onMessage.addListener(message => {
+        if (message.action == 'overrideElement') {
+            try {
+                const input = document.getElementById('prompt-textarea');
+                input.focus();
+                input.value = message.prompt.replace(/^[ \t]+/gm, '').trim();
+                document.execCommand('selectAll', false, null);
+                document.execCommand('insertText', false, input.value);
+            } catch (e) {
+                if (e.message.indexOf('Cannot read properties of null') != -1) {
+                    alert('Input field not found.\nPlease ensure you are on the official ChatGPT page.');
+                }
+            }
+        }
+    });
+}
